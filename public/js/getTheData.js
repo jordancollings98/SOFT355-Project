3e0bbdc45233b5c373ab2eac35b5c12f7e2413a6
Jordan <ifailz@hotmail.co.uk>
2020-01-06 13:30:05 +0000

    Add files via upload

--- public/js/getTheData.js
+++ public/js/getTheData.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
   $("#btnGetData").click(function(){
-    $('#rowData').html("");
-    $('#rowData2').html("");
+    $('#getTitleData').html("");
+    $('#getMessageData').html("");
+    $('#getIdData').html("");
     $.getJSON("/getNotes", function(result){
-      var noteTitle = '<h5>' + "Title" + '</h5>';
-      var messageTitle = '<h5>' + "Message" + '</h5>';
+      var noteTitle = '<h3 id="getNoteTitle">' + "Title" + '</h3>';
+      var messageTitle = '<h3 id="getNoteTitle">' + "Message" + '</h3>';
+      var idTitle = '<h3 id="getNoteTitle">' + "ID" + '</h3>';
 
-        $('#rowData').append(noteTitle);
-        $('#rowData2').append(messageTitle);
+        $('#getTitleData').append(noteTitle);
+        $('#getMessageData').append(messageTitle);
+        $('#getIdData').append(idTitle);
 
 
       $.each(result, function(i, note){
         var option_result = '<li id="titleList">'+ note.title + '</li>';
-        var option_result2 =   '<li id="messageList">'+ note.message + '</li>';
-          $('#rowData').append(option_result);
-          $('#rowData2').append(option_result2);
+        var option_result2 = '<li id="messageList">'+ note.message + '</li>';
+        var option_result3 = '<li id="titleList">' + note._id + '</li>';
+          $('#getTitleData').append(option_result);
+          $('#getMessageData').append(option_result2);
+          $('#getIdData').append(option_result3);
       });
     });
   });
